db0cf6ce0c0c80fcc414a9b33f089a655730f4b5
Ethan Paul <24588726+enpaul@users.noreply.github.com>
2020-10-24 11:08:47 -0400

    Bump patch version

--- tox_poetry_installer.py
+++ tox_poetry_installer.py
@@ -45,7 +45,7 @@ from tox.venv import VirtualEnv as ToxVirtualEnv
 
 __title__ = "tox-poetry-installer"
 __summary__ = "Tox plugin to install Tox environment dependencies using the Poetry backend and lockfile"
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __url__ = "https://github.com/enpaul/tox-poetry-installer/"
 __license__ = "MIT"
 __authors__ = ["Ethan Paul <24588726+enpaul@users.noreply.github.com>"]
