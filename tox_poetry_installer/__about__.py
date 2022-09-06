5f30656683c90919cbe495d06fe32be4794b0484
Ethan Paul <24588726+enpaul@users.noreply.github.com>
2022-09-06 15:08:01 -0400

    Fix poetry dependency spec to <1.2 for last 0.8 release
    Update pylint to >2.13 to fix security warning

--- tox_poetry_installer/__about__.py
+++ tox_poetry_installer/__about__.py
@@ -1,7 +1,7 @@
 # pylint: disable=missing-docstring
 __title__ = "tox-poetry-installer"
 __summary__ = "A plugin for Tox that lets you install test environment dependencies from the Poetry lockfile"
-__version__ = "0.8.4"
+__version__ = "0.8.5"
 __url__ = "https://github.com/enpaul/tox-poetry-installer/"
 __license__ = "MIT"
 __authors__ = ["Ethan Paul <24588726+enpaul@users.noreply.github.com>"]
